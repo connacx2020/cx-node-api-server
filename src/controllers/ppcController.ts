b738fbdcf1b1bfd4847a75c720bf882cb00a4b38
Thet Htet Aung <thethtetaung17@gmail.com>
2020-07-26 16:04:23 +0630

    ppc api for all entrance

--- src/controllers/ppcController.ts
+++ src/controllers/ppcController.ts
@@ -1,9 +1,91 @@
-exports.ppc = function (req: any, res: any) {
-    res.json({
-        status: "success",
-        message: "ppc controller",
-        data: {
-            name: "Sample"
+import pool from '../utils/dbClient';
+
+exports.getAllEntranceMonthlyPPC = function (req: any, res: any) {
+    pool.query(`select entity_id, key as door, long_v as _data, 
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::date as datetime,
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::time as _time
+    from ts_kv_latest where entity_id in ($1, $2) and
+    key IN  ('visitor_count_door1','visitor_count_door2', 'visitor_count_door3', 'visitor_count_door4') 
+    order by datetime desc`, ['1ea296c156d41b083816530eccc01ed', '1ea297011afa0a083816530eccc01ed'], (error: any, results: any) => {
+        if (error) {
+            throw error;
         }
+        res.status(200).json({
+            status: 200,
+            message: 'Successful!',
+            data: results.rows
+        }).end();
+    });
+}
+
+exports.getEastEntranceMonthlyPPC = (req: any, res: any) => {
+    pool.query(`select entity_id, key as door, long_v as _data, 
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::date as datetime,
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::time as _time
+    from ts_kv_latest where entity_id in ($1, $2) and
+    key IN  ('visitor_count_door1') 
+    order by datetime desc`, ['1ea296c156d41b083816530eccc01ed', '1ea297011afa0a083816530eccc01ed'], (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json({
+            status: 200,
+            message: 'Successful!',
+            data: results.rows
+        }).end();
+    });
+}
+
+exports.getWestEntranceMonthlyPPC = (req: any, res: any) => {
+    pool.query(`select entity_id, key as door, long_v as _data, 
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::date as datetime,
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::time as _time
+    from ts_kv_latest where entity_id in ($1, $2) and
+    key IN  ('visitor_count_door2') 
+    order by datetime desc`, ['1ea296c156d41b083816530eccc01ed', '1ea297011afa0a083816530eccc01ed'], (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json({
+            status: 200,
+            message: 'Successful!',
+            data: results.rows
+        }).end();
+    });
+}
+
+exports.getCircleEntranceMonthlyPPC = (req: any, res: any) => {
+    pool.query(`select entity_id, key as door, long_v as _data, 
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::date as datetime,
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::time as _time
+    from ts_kv_latest where entity_id in ($1, $2) and
+    key IN  ('visitor_count_door3') 
+    order by datetime desc`, ['1ea296c156d41b083816530eccc01ed', '1ea297011afa0a083816530eccc01ed'], (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json({
+            status: 200,
+            message: 'Successful!',
+            data: results.rows
+        }).end();
+    });
+}
+
+exports.getB2EntranceMonthlyPPC = (req: any, res: any) => {
+    pool.query(`select entity_id, key as door, long_v as _data, 
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::date as datetime,
+    TO_TIMESTAMP( TRUNC( CAST( ts AS bigint ) / 1000 ))::time as _time
+    from ts_kv_latest where entity_id in ($1, $2) and
+    key IN  ('visitor_count_door4') 
+    order by datetime desc`, ['1ea296c156d41b083816530eccc01ed', '1ea297011afa0a083816530eccc01ed'], (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json({
+            status: 200,
+            message: 'Successful!',
+            data: results.rows
+        }).end();
     });
 }
--- src/routes/ppcRoutes.ts
+++ src/routes/ppcRoutes.ts
@@ -1,8 +1,20 @@
 import * as express from 'express';
-var controller =  require('../controllers/ppcController');
+var controller = require('../controllers/ppcController');
 
 var router = express.Router();
-router.route('/ppc')
-    .get(controller.ppc);
+router.route('/getAllEntranceMonthlyPPC')
+    .get(controller.getAllEntranceMonthlyPPC);
+
+router.route('/getEastEntranceMonthlyPPC')
+    .get(controller.getEastEntranceMonthlyPPC);
+
+router.route('/getWestEntranceMonthlyPPC')
+    .get(controller.getWestEntranceMonthlyPPC);
+
+router.route('/getCircleEntranceMonthlyPPC')
+    .get(controller.getCircleEntranceMonthlyPPC);
+
+router.route('/getB2EntranceMonthlyPPC')
+    .get(controller.getB2EntranceMonthlyPPC);
 
 module.exports = router;
