97c0fd1a1f315d17863d32dcce455e65b565a9b8
Thet Htet Aung <thethtetaung17@gmail.com>
2020-08-18 12:22:18 +0630

    Checking with date and also time

--- src/routes/magRoutes.ts
+++ src/routes/magRoutes.ts
@@ -5,7 +5,7 @@ const { authenticateJWT } = require('../utils/jwt');
 var router = express.Router();
 
 router.route('/getMAGByDateTimeRange')
-    // .get(authenticateJWT)
+    .get(authenticateJWT)
     .get(controller.getMAGByDateTimeRange);
 
 module.exports = router;
