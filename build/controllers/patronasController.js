ed61016e919a12d1c74c71dcc3f112026f93e276
Thet Htet Aung <thethtetaung17@gmail.com>
2020-09-06 17:11:48 +0630

    Check key with key_id

--- build/controllers/patronasController.js
+++ build/controllers/patronasController.js
@@ -7,10 +7,88 @@ exports.getPatronasData = (req, res) => {
         if (error) {
             throw error;
         }
+        const data = results.rows;
+        const returnData = [];
+        data.forEach((res) => {
+            switch (res.key) {
+                case 7: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "count",
+                        data: res.long_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 8: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump1_data",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 9: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump2_data",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 10: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump3_data",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 11: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump4_data",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 12: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump5_data",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 13: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump6_data",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+                case 14: {
+                    returnData.push({
+                        entity_id: res.entity_id,
+                        key: "pump1",
+                        data: res.json_v,
+                        ts: res.ts
+                    });
+                    break;
+                }
+            }
+        });
         res.status(200).json({
             status: 200,
             message: 'Successful!',
-            data: results.rows
+            data: returnData
         }).end();
     });
 };
--- src/controllers/patronasController.ts
+++ src/controllers/patronasController.ts
@@ -4,13 +4,91 @@ exports.getPatronasData = (req: any, res: any) => {
     const deviceID = "f62241e0-edc2-11ea-a72f-7398ea06dc89";
     pool.query(
         `select * from ts_kv where entity_id=$1 order by key;`, [deviceID], (error: any, results: any) => {
-        if (error) {
-            throw error;
-        }
-        res.status(200).json({
-            status: 200,
-            message: 'Successful!',
-            data: results.rows
-        }).end();
-    });
+            if (error) {
+                throw error;
+            }
+            const data = results.rows;
+            const returnData: any = [];
+            data.forEach((res: any) => {
+                switch (res.key) {
+                    case 7: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "count",
+                            data: res.long_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 8: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump1_data",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 9: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump2_data",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 10: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump3_data",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 11: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump4_data",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 12: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump5_data",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 13: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump6_data",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                    case 14: {
+                        returnData.push({
+                            entity_id: res.entity_id,
+                            key: "pump1",
+                            data: res.json_v,
+                            ts: res.ts
+                        });
+                        break;
+                    }
+                }
+            });
+            res.status(200).json({
+                status: 200,
+                message: 'Successful!',
+                data: returnData
+            }).end();
+        });
 }
