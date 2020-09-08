8c8f4d0610b03f2b22fb0a56132e5f2ab5064041
Thet Htet Aung <thethtetaung17@gmail.com>
2020-09-08 12:37:33 +0630

    Query for patronas from thingsboard

--- build/controllers/patronasController.js
+++ build/controllers/patronasController.js
@@ -7,12 +7,20 @@ exports.getPatronasData = (req, res) => {
     const datetime1 = `${date} 07:00:00`;
     const datetime2 = `${date} 22:00:00`;
     dbClient_1.default.query(`
-        SELECT *
+        SELECT CASE
+        WHEN key = '8' THEN 'pump1'
+        WHEN key = '9' THEN 'pump2'
+        WHEN key = '10' THEN 'pump3'
+        WHEN key='11' THEN 'pump4'
+        WHEN key='12' THEN 'pump5'
+        WHEN key='13' THEN 'pump6'
+        END AS "KEY", json_v, ts, long_v, datetime
         FROM 
         (select entity_id, key, long_v, json_v, ts,
-        TO_TIMESTAMP(TRUNC(ts/1000)) + INTERVAL '8 hour' as datetime
+        TO_TIMESTAMP(TRUNC(ts/1000)) as datetime
         from ts_kv 
-        where entity_id=$1) AS expr_qry
+        where entity_id=$1 
+        AND key IN ('8', '9', '10', '11', '12', '13')) AS expr_qry
         WHERE datetime >= $2
         AND datetime <= $3
         ORDER BY ts;
@@ -21,88 +29,100 @@ exports.getPatronasData = (req, res) => {
             throw error;
         }
         const data = results.rows;
-        console.log(data);
-        const returnData = [];
-        data.forEach((res) => {
-            switch (res.key) {
-                case 7: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "count",
-                        data: res.long_v,
-                        ts: res.ts
-                    });
-                    break;
+        const startTime = 13;
+        const endTime = 15;
+        var binCount = endTime - startTime;
+        var pump = {
+            pump1: [],
+            pump2: [],
+            pump3: [],
+            pump4: [],
+            pump5: [],
+            pump6: []
+        };
+        for (var i = 0; i < binCount; i++) {
+            const bin = {
+                vehicleType1: {
+                    count: 0,
+                    dwellTime: 0
+                },
+                vehicleType2: {
+                    count: 0,
+                    dwellTime: 0
+                },
+                vehicleType3: {
+                    count: 0,
+                    dwellTime: 0
+                },
+                vehicleType4: {
+                    count: 0,
+                    dwellTime: 0
                 }
-                case 8: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump1_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
+            };
+            pump.pump1.push(bin);
+            pump.pump2.push(bin);
+            pump.pump3.push(bin);
+            pump.pump4.push(bin);
+            pump.pump5.push(bin);
+            pump.pump6.push(bin);
+        }
+        console.log(pump);
+        for (var i = 0; i < binCount; i++) {
+            const start = startTime + i;
+            const time1 = new Date(`${date} ${start}:00:00`);
+            const time2 = new Date(time1.getTime() + 3600 * 1000);
+            data.forEach((pumpData) => {
+                if (pumpData.datetime >= time1 && pumpData.datetime < time2) {
+                    switch (pumpData.KEY) {
+                        case 'pump1': {
+                            const pump1Data = pump.pump1;
+                            const count = pumpData.json_v.count;
+                            const dwellTime = pumpData.json_v.time;
+                            const vehicleType = pumpData.json_v.vehicleType;
+                            if (vehicleType == 'type1') {
+                                pump1Data[i].vehicleType1.count += count;
+                                pump1Data[i].vehicleType1.dwellTime += dwellTime;
+                            }
+                            else if (vehicleType == 'type2') {
+                                pump1Data[i].vehicleType2.count += count;
+                                pump1Data[i].vehicleType2.dwellTime += dwellTime;
+                            }
+                            else if (vehicleType == 'type3') {
+                                pump1Data[i].vehicleType3.count += count;
+                                pump1Data[i].vehicleType3.dwellTime += dwellTime;
+                            }
+                            else if (vehicleType == 'type4') {
+                                pump1Data[i].vehicleType4.count += count;
+                                pump1Data[i].vehicleType4.dwellTime += dwellTime;
+                            }
+                            console.log(pump1Data);
+                            break;
+                        }
+                        case 'pump2': {
+                            const pump2Data = pump.pump2;
+                            console.log("Pump2", pump2Data);
+                            break;
+                        }
+                        case 'pump3': {
+                            break;
+                        }
+                        case 'pump4': {
+                            break;
+                        }
+                        case 'pump5': {
+                            break;
+                        }
+                        case 'pump6': {
+                            break;
+                        }
+                    }
                 }
-                case 9: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump2_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 10: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump3_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 11: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump4_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 12: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump5_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 13: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump6_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 14: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump1",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-            }
-        });
+            });
+        }
         res.status(200).json({
             status: 200,
             message: 'Successful!',
-            data: returnData
+            pumpData: pump
         }).end();
     });
 };
--- src/controllers/patronasController.ts
+++ src/controllers/patronasController.ts
@@ -8,12 +8,20 @@ exports.getPatronasData = (req: any, res: any) => {
     const datetime2 = `${date} 22:00:00`;
     pool.query(
         `
-        SELECT *
+        SELECT CASE
+        WHEN key = '8' THEN 'pump1'
+        WHEN key = '9' THEN 'pump2'
+        WHEN key = '10' THEN 'pump3'
+        WHEN key='11' THEN 'pump4'
+        WHEN key='12' THEN 'pump5'
+        WHEN key='13' THEN 'pump6'
+        END AS "KEY", json_v, ts, long_v, datetime
         FROM 
         (select entity_id, key, long_v, json_v, ts,
-        TO_TIMESTAMP(TRUNC(ts/1000)) + INTERVAL '8 hour' as datetime
+        TO_TIMESTAMP(TRUNC(ts/1000)) as datetime
         from ts_kv 
-        where entity_id=$1) AS expr_qry
+        where entity_id=$1 
+        AND key IN ('8', '9', '10', '11', '12', '13')) AS expr_qry
         WHERE datetime >= $2
         AND datetime <= $3
         ORDER BY ts;
@@ -22,88 +30,127 @@ exports.getPatronasData = (req: any, res: any) => {
             throw error;
         }
         const data = results.rows;
-        console.log(data);
-        const returnData: any = [];
-        data.forEach((res: any) => {
-            switch (res.key) {
-                case 7: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "count",
-                        data: res.long_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 8: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump1_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 9: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump2_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 10: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump3_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 11: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump4_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 12: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump5_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 13: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump6_data",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
-                }
-                case 14: {
-                    returnData.push({
-                        entity_id: res.entity_id,
-                        key: "pump1",
-                        data: res.json_v,
-                        ts: res.ts
-                    });
-                    break;
+        const startTime = 7;
+        const endTime = 22;
+        var binCount = endTime - startTime;
+        var pump: any = {
+            pump1: [],
+            pump2: [],
+            pump3: [],
+            pump4: [],
+            pump5: [],
+            pump6: []
+        };
+
+        const calculateVehicleCount = (vehicleType: string, count: number, time: any, datetime: any, time1: any, time2: any) => {
+            const bin = {
+                vehicleType1: {
+                    count: 0,
+                    dwellTime: 0
+                },
+                vehicleType2: {
+                    count: 0,
+                    dwellTime: 0
+                },
+                vehicleType3: {
+                    count: 0,
+                    dwellTime: 0
+                },
+                vehicleType4: {
+                    count: 0,
+                    dwellTime: 0
                 }
             }
-        });
+
+            if (datetime >= time1 && datetime < time2) {
+                switch (vehicleType) {
+                    case 'type1': {
+                        bin.vehicleType1.count += count;
+                        bin.vehicleType1.dwellTime += time;
+                        break;
+                    }
+                    case 'type2': {
+                        bin.vehicleType2.count += count;
+                        bin.vehicleType2.dwellTime += time;
+                        break;
+                    }
+                    case 'type3': {
+                        bin.vehicleType3.count += count;
+                        bin.vehicleType3.dwellTime += time;
+                        break;
+                    }
+                    case 'type4': {
+                        bin.vehicleType4.count += count;
+                        bin.vehicleType4.dwellTime += time;
+                        break;
+                    }
+                    default: break;
+                }
+            }
+            return bin;
+        }
+        for (var i = 0; i < binCount; i++) {
+            const start = startTime + i;
+            const time1 = new Date(`${date} ${start}:00:00`);
+            const time2 = new Date(time1.getTime() + 3600 * 1000);
+
+            for (var pmpData of data) {
+                const vehicleType = pmpData['json_v'].vehicleType;
+                const count = pmpData['json_v'].count;
+                var dwellTime = pmpData['json_v'].time;
+                var dwellTimeSplited = dwellTime.split(' ');
+                if (dwellTimeSplited.length > 2) {
+                    if(dwellTimeSplited[1] == 'mins' && dwellTimeSplited[3] == 'secs') {
+                        dwellTime = Number(dwellTimeSplited[0])*60 + Number(dwellTimeSplited[2]);
+                    }
+                } else {
+                    if(dwellTimeSplited[1] == 'mins') {
+                        dwellTime = Number(dwellTimeSplited[0])*60;
+                    } else {
+                        dwellTime = Number(dwellTimeSplited[0]);
+                    }
+                }
+                const datetime = pmpData.datetime;
+                const bin = calculateVehicleCount(vehicleType, count, dwellTime, datetime, time1, time2);
+                switch (pmpData.KEY) {
+                    case 'pump1': {
+                        const bin = calculateVehicleCount(vehicleType, count, dwellTime, datetime, time1, time2);
+                        pump.pump1.push(bin);
+                        break;
+                    }
+                    case 'pump2':
+                        {
+                            pump.pump2.push(bin);
+                            break;
+                        }
+                    case 'pump3':
+                        {
+                            pump.pump3.push(bin);
+                            break;
+                        }
+                    case 'pump4':
+                        {
+                            pump.pump4.push(bin);
+                            break;
+                        }
+                    case 'pump5':
+                        {
+                            pump.pump5.push(bin);
+                            break;
+                        }
+                    case 'pump6': {
+                        pump.pump6.push(bin);
+                        break;
+                    }
+                    default: break;
+                }
+            }
+        }
+
         res.status(200).json({
             status: 200,
             message: 'Successful!',
-            data: returnData
+            pumpData: pump
         }).end();
     });
 }
