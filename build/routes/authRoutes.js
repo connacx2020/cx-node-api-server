7e95f8f036dc5bfff2ccfa09471e174b9e852976
Thet Htet Aung <thethtetaung@Thets-MacBook-Pro.local>
2021-02-04 09:42:18 +0630

    Publika api branchout

--- build/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-var controller = require('../controllers/authController');
-var router = express.Router();
-router.route('/login')
-    .post(controller.login);
-module.exports = router;
